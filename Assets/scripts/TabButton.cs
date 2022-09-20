f2f757dbdc7179477654ef3dd236bc477e566379
Till <t.buchtal@googlemail.com>
2022-09-20 16:37:12 +0200

    Started working on the Tab buttons
    hope this works

---
+++ Assets/scripts/TabButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class TabButton : Entity
+{
+    public TabGroup tabGroup;
+    public Image background;
+
+    private void Start() 
+    {
+        cords = Utilities.ConvertCordsToInt(transform.position);
+        background = GetComponent<Image>();
+        tabGroup.Subscribe(this);
+    }
+
+    public override void MouseEnter(Vector3Int cords, List<Vector3Int> shape)
+    {
+        base.MouseEnter(cords, shape);
+    }
+
+    public override void MouseExit(Vector3Int cords, List<Vector3Int> shape)
+    {
+        base.MouseExit(cords, shape);
+    }
+
+    public override void MouseInteractionLeft(Vector3Int cords)
+    {
+        base.MouseInteractionLeft(cords);
+    }
+
+}
